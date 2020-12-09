d90cad0ae4aac79aba32a60c16d818bd7af0f772
Eugene Judith <s6raphin@laposte.net>
2020-12-09 19:31:14 +0100

    #6 Change default DB user/password
    Delete auto-formating

--- src/main/java/com/parkit/parkingsystem/config/DataBaseConfig.java
+++ src/main/java/com/parkit/parkingsystem/config/DataBaseConfig.java
@@ -1,13 +1,9 @@
 package com.parkit.parkingsystem.config;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.sql.*;
 
 public class DataBaseConfig {
 
@@ -24,36 +20,36 @@ public class DataBaseConfig {
 	return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
     }
 
-    public void closeConnection(Connection con) {
-	if (con != null) {
-	    try {
-		con.close();
-		logger.info("Closing DB connection");
-	    } catch (SQLException e) {
-		logger.error("Error while closing connection", e);
-	    }
-	}
+    public void closeConnection(Connection con){
+        if(con!=null){
+            try {
+                con.close();
+                logger.info("Closing DB connection");
+            } catch (SQLException e) {
+                logger.error("Error while closing connection",e);
+            }
+        }
     }
 
     public void closePreparedStatement(PreparedStatement ps) {
-	if (ps != null) {
-	    try {
-		ps.close();
-		logger.info("Closing Prepared Statement");
-	    } catch (SQLException e) {
-		logger.error("Error while closing prepared statement", e);
-	    }
-	}
+        if(ps!=null){
+            try {
+                ps.close();
+                logger.info("Closing Prepared Statement");
+            } catch (SQLException e) {
+                logger.error("Error while closing prepared statement",e);
+            }
+        }
     }
 
     public void closeResultSet(ResultSet rs) {
-	if (rs != null) {
-	    try {
-		rs.close();
-		logger.info("Closing Result Set");
-	    } catch (SQLException e) {
-		logger.error("Error while closing result set", e);
-	    }
-	}
+        if(rs!=null){
+            try {
+                rs.close();
+                logger.info("Closing Result Set");
+            } catch (SQLException e) {
+                logger.error("Error while closing result set",e);
+            }
+        }
     }
 }
--- src/test/java/com/parkit/parkingsystem/integration/config/DataBaseTestConfig.java
+++ src/test/java/com/parkit/parkingsystem/integration/config/DataBaseTestConfig.java
@@ -1,15 +1,10 @@
 package com.parkit.parkingsystem.integration.config;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
+import com.parkit.parkingsystem.config.DataBaseConfig;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import com.parkit.parkingsystem.config.DataBaseConfig;
+import java.sql.*;
 
 public class DataBaseTestConfig extends DataBaseConfig {
 
@@ -21,36 +16,36 @@ public class DataBaseTestConfig extends DataBaseConfig {
 	return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
     }
 
-    public void closeConnection(Connection con) {
-	if (con != null) {
-	    try {
-		con.close();
-		logger.info("Closing DB connection");
-	    } catch (SQLException e) {
-		logger.error("Error while closing connection", e);
-	    }
-	}
+    public void closeConnection(Connection con){
+        if(con!=null){
+            try {
+                con.close();
+                logger.info("Closing DB connection");
+            } catch (SQLException e) {
+                logger.error("Error while closing connection",e);
+            }
+        }
     }
 
     public void closePreparedStatement(PreparedStatement ps) {
-	if (ps != null) {
-	    try {
-		ps.close();
-		logger.info("Closing Prepared Statement");
-	    } catch (SQLException e) {
-		logger.error("Error while closing prepared statement", e);
-	    }
-	}
+        if(ps!=null){
+            try {
+                ps.close();
+                logger.info("Closing Prepared Statement");
+            } catch (SQLException e) {
+                logger.error("Error while closing prepared statement",e);
+            }
+        }
     }
 
     public void closeResultSet(ResultSet rs) {
-	if (rs != null) {
-	    try {
-		rs.close();
-		logger.info("Closing Result Set");
-	    } catch (SQLException e) {
-		logger.error("Error while closing result set", e);
-	    }
-	}
+        if(rs!=null){
+            try {
+                rs.close();
+                logger.info("Closing Result Set");
+            } catch (SQLException e) {
+                logger.error("Error while closing result set",e);
+            }
+        }
     }
 }
