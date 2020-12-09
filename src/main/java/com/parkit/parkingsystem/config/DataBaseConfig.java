cc6ffa6e9f6aab876df32a073c5fa50b1f918708
Eugene Judith <s6raphin@laposte.net>
2020-12-09 19:21:12 +0100

    #6 Change default DB user/password #6
    Change default credentials for main and tests scope.

--- src/main/java/com/parkit/parkingsystem/config/DataBaseConfig.java
+++ src/main/java/com/parkit/parkingsystem/config/DataBaseConfig.java
@@ -1,51 +1,59 @@
 package com.parkit.parkingsystem.config;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.sql.*;
 
 public class DataBaseConfig {
 
     private static final Logger logger = LogManager.getLogger("DataBaseConfig");
 
+    protected static final String DB_SCHEMA = "prod";
+    protected static final String DB_URL = "jdbc:mysql://localhost:3306/" + DB_SCHEMA;
+    protected static final String DB_USER = "p4";
+    protected static final String DB_PASSWORD = "password";
+
     public Connection getConnection() throws ClassNotFoundException, SQLException {
-        logger.info("Create DB connection");
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        return DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/prod","root","rootroot");
+	logger.info("Create DB connection");
+	Class.forName("com.mysql.cj.jdbc.Driver");
+	return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
     }
 
-    public void closeConnection(Connection con){
-        if(con!=null){
-            try {
-                con.close();
-                logger.info("Closing DB connection");
-            } catch (SQLException e) {
-                logger.error("Error while closing connection",e);
-            }
-        }
+    public void closeConnection(Connection con) {
+	if (con != null) {
+	    try {
+		con.close();
+		logger.info("Closing DB connection");
+	    } catch (SQLException e) {
+		logger.error("Error while closing connection", e);
+	    }
+	}
     }
 
     public void closePreparedStatement(PreparedStatement ps) {
-        if(ps!=null){
-            try {
-                ps.close();
-                logger.info("Closing Prepared Statement");
-            } catch (SQLException e) {
-                logger.error("Error while closing prepared statement",e);
-            }
-        }
+	if (ps != null) {
+	    try {
+		ps.close();
+		logger.info("Closing Prepared Statement");
+	    } catch (SQLException e) {
+		logger.error("Error while closing prepared statement", e);
+	    }
+	}
     }
 
     public void closeResultSet(ResultSet rs) {
-        if(rs!=null){
-            try {
-                rs.close();
-                logger.info("Closing Result Set");
-            } catch (SQLException e) {
-                logger.error("Error while closing result set",e);
-            }
-        }
+	if (rs != null) {
+	    try {
+		rs.close();
+		logger.info("Closing Result Set");
+	    } catch (SQLException e) {
+		logger.error("Error while closing result set", e);
+	    }
+	}
     }
 }
--- src/test/java/com/parkit/parkingsystem/integration/config/DataBaseTestConfig.java
+++ src/test/java/com/parkit/parkingsystem/integration/config/DataBaseTestConfig.java
@@ -1,52 +1,56 @@
 package com.parkit.parkingsystem.integration.config;
 
-import com.parkit.parkingsystem.config.DataBaseConfig;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.sql.*;
+import com.parkit.parkingsystem.config.DataBaseConfig;
 
 public class DataBaseTestConfig extends DataBaseConfig {
 
     private static final Logger logger = LogManager.getLogger("DataBaseTestConfig");
 
     public Connection getConnection() throws ClassNotFoundException, SQLException {
-        logger.info("Create DB connection");
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        return DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/test","root","rootroot");
+	logger.info("Create DB connection");
+	Class.forName("com.mysql.cj.jdbc.Driver");
+	return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
     }
 
-    public void closeConnection(Connection con){
-        if(con!=null){
-            try {
-                con.close();
-                logger.info("Closing DB connection");
-            } catch (SQLException e) {
-                logger.error("Error while closing connection",e);
-            }
-        }
+    public void closeConnection(Connection con) {
+	if (con != null) {
+	    try {
+		con.close();
+		logger.info("Closing DB connection");
+	    } catch (SQLException e) {
+		logger.error("Error while closing connection", e);
+	    }
+	}
     }
 
     public void closePreparedStatement(PreparedStatement ps) {
-        if(ps!=null){
-            try {
-                ps.close();
-                logger.info("Closing Prepared Statement");
-            } catch (SQLException e) {
-                logger.error("Error while closing prepared statement",e);
-            }
-        }
+	if (ps != null) {
+	    try {
+		ps.close();
+		logger.info("Closing Prepared Statement");
+	    } catch (SQLException e) {
+		logger.error("Error while closing prepared statement", e);
+	    }
+	}
     }
 
     public void closeResultSet(ResultSet rs) {
-        if(rs!=null){
-            try {
-                rs.close();
-                logger.info("Closing Result Set");
-            } catch (SQLException e) {
-                logger.error("Error while closing result set",e);
-            }
-        }
+	if (rs != null) {
+	    try {
+		rs.close();
+		logger.info("Closing Result Set");
+	    } catch (SQLException e) {
+		logger.error("Error while closing result set", e);
+	    }
+	}
     }
 }
