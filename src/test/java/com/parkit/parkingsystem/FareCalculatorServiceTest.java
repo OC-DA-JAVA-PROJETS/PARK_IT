ff6ac29cc94b53d6ae2977581d1326da44bb715d
s6raphin <s6raphin@laposte.net>
2021-01-13 21:36:36 +0100

    #2_5%-discountforrecurringusers
    Add Mockito extension on test class (resolve infinite waiting on when calls)

--- src/test/java/com/parkit/parkingsystem/FareCalculatorServiceTest.java
+++ src/test/java/com/parkit/parkingsystem/FareCalculatorServiceTest.java
@@ -13,7 +13,9 @@ import com.parkit.parkingsystem.util.InputReaderUtil;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Date;
 
@@ -22,6 +24,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class FareCalculatorServiceTest {
 
     private static FareCalculatorService fareCalculatorService;
